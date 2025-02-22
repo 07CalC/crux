37066e929cd957bfc1613cbb79136c2c71c9ee1a
CalC <maheshwarivinayak90@gmail.com>
2025-02-22 20:17:52 +0530

    updated scraper script

--- utils/tableScraper.js
+++ utils/tableScraper.js
@@ -1,5 +1,6 @@
 // Usage: go to jossa official prev years orcr select the year and round number
 // then run this script in the console and copy the array returned
+// note the table is const here so you might have to refresh the page after scraping every round's data
 const table = document.querySelector("table");
 const data = [];
 
@@ -8,8 +9,19 @@ if (table) {
         if (rowIndex === 0) return;
         const cols = row.querySelectorAll("td");
         const rowData = Array.from(cols).map(col => col.innerText.trim());
-        data.push(rowData);
+
+        data.push({
+            institute: rowData[0],
+            academicProgramName: rowData[1],
+            quota: rowData[2],
+            seatType: rowData[3],
+            gender: rowData[4],
+            openRank: rowData[5],
+            closeRank: rowData[6]
+        });
     });
 
     console.log(data);
-}
+} else {
+    console.log("Table not found on the page.");
+} 

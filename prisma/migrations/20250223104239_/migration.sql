9ffb7fe65805bb5afc99ceae4b3f26413a29f7c8
CalC <maheshwarivinayak90@gmail.com>
2025-03-06 23:11:33 +0530

    switched to sqlite

--- prisma/migrations/20250223104239_/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "College" ALTER COLUMN "coverImage" DROP NOT NULL,
-ALTER COLUMN "location" DROP NOT NULL,
-ALTER COLUMN "officialWebsite" DROP NOT NULL;
--- prisma/migrations/20250223172158_maybe_final/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "College" ALTER COLUMN "femaleStudents" DROP NOT NULL,
-ALTER COLUMN "maleStudents" DROP NOT NULL,
-ALTER COLUMN "totalStudents" DROP NOT NULL;

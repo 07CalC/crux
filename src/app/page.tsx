3effd628e7d5c01bc5a123438c8aca348df148ec
07CalC <maheshwarivinayak90@gmail.com>
2025-04-19 12:48:20 +0530

    prod ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import { redirect } from "next/navigation";
 
 
 export default async function Home() {
+  redirect('orcr')
   return (
     <div className="flex flex-col py-10 min-h-screen w-full h-full gap-y-4 justify-center items-center">
         

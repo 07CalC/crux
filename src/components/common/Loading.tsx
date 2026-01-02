794f8b79d72ff1ec01ba9893e55c155c3c2d4546
07calc <maheshwarivinayak90@gmail.com>
2026-01-02 23:41:54 +0530

    fix Image warning

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -5,7 +5,7 @@ export const Loading = () => {
     return (
         <div className="flex flex-col py-10 w-full gap-y-4 justify-center items-center">
             <p className="text-black font-semibold text-3xl sm:text-4xl dark:text-white">Loading...</p>
-            <Image src="/loadingCat2.gif" alt="loading" width={100} height={100} className="w-4/6 sm:w-1/4 " />
+            <Image unoptimized src="/loadingCat2.gif" alt="loading" width={100} height={100} className="w-4/6 sm:w-1/4 " />
 
         </div>
     )
